8ccd4d8a9462cdf6f5a1eb62e0eff4ac1de18bea
zethill <fornaridarian@gmail.com>
2020-12-18 16:00:52 +0100

    ..

--- ChiakiPi/ChiakiPi.bat
+++
@@ -1,8 +0,0 @@
-rem Ce fichier de commande a ‚t‚ g‚n‚r‚ par WINDEV lors de l'exportation Java du projet <Chiaki-PI>.
-rem Il peut ˆtre utilis‚ pour ex‚cuter votre archive Java (Jar) si le double-clic sur l'archive ne lance pas son ex‚cution.
-rem ATTENTION : Vous devrez adapter la commande ci-dessous pour pouvoir l'utiliser sur une autre machine ou un autre OS.
-
-"C:\Program Files\Java\jdk-15.0.1\bin\java.exe" -classpath ChiakiPi.jar GWDPChiaki_PI
-
-rem La ligne suivante bloque la fermeture automatique de la console en cas d'erreur : 
-if errorlevel 1 pause
